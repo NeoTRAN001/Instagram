f420cd707f6599b3131591cf176f14dc50bd2ea4
NeoTRAN001 <neo_tran@outlook.com>
2020-12-31 23:12:44 -0600

    GraphQL with apollo server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,27 @@
 const mongoose = require('mongoose');
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./gql/schema');
+const resolvers = require('./gql/resolver');
+require('dotenv').config({ path: ".env" });
+
+// Config Server Connection
+mongoose.connect(process.env.BBDD, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useCreateIndex: true
+}, (err, _) => {
+    if(err) console.log('Error connection');
+    else server();
+});
+
+// Server On with apollo and Schema, Resolver files
+function server() {
+    const serverApollo = new ApolloServer({
+        typeDefs, resolvers
+    });
+
+    serverApollo.listen().then(({ url }) => {
+        console.log(`Server ON in: ${url}`)
+    });
+}
