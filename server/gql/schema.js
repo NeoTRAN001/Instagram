0daa58b84e25fac163ab37eee53a022564ebc414
NeoTRAN001 <neo_tran@outlook.com>
2021-01-02 00:06:37 -0600

    Add Token and LOgin

--- server/gql/schema.js
+++ server/gql/schema.js
@@ -4,7 +4,7 @@ const typeDefs = gql`
     type User {
         id:          ID
         name:        String
-        username:     String
+        username:    String
         email:       String
         siteWeb:     String
         description: String
@@ -13,10 +13,19 @@ const typeDefs = gql`
         createAt:    String
     }
 
+    type Token {
+        token: String
+    }
+
     input UserInput {
         name:     String! # ! = Require
         username: String!
         email:    String!
+        password: String!
+    }
+
+    input LoginInput {
+        email: String!
         password: String!
     }
 
@@ -28,6 +37,7 @@ const typeDefs = gql`
     type Mutation {
         # User
         register(input: UserInput): User
+        login(input: LoginInput): Token
     }
 `;
 
