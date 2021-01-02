daa4b5c4c4c019b97ee3fbb511b63e612c4169e2
NeoTRAN001 <neo_tran@outlook.com>
2021-01-02 00:06:17 -0600

    Add Login, and move logic to controller

--- server/gql/resolver.js
+++ server/gql/resolver.js
@@ -1,43 +1,15 @@
-const User = require('../models/user');
-const bcryptjs = require('bcryptjs');
-
-function lowerCaseAndNoSpace(text) {
-    return text.trim().toLowerCase();
-}
+const userController = require('../controllers/user');
+const user = require('../models/user');
 
 const resolver = {
     Query: {
         // User
-        getUser: () => {
-            console.log('Get Users :D');
-            return null;
-        }
+        getUser: () => userController.getUser(),
     },
     Mutation: {
         // User
-        register: async (_, { input }) => {
-            const newUser = input;
-            newUser.email = lowerCaseAndNoSpace(newUser.email);
-            newUser.username = lowerCaseAndNoSpace(newUser.username);
-
-            const { email, username, password } = newUser;
-            const foundEmail = await User.findOne({ email }); // Validate if email exists
-            const foundUsername = await User.findOne({ username }); // Validate if username exists
-
-            if(foundEmail) throw Error("The email already exists");
-            if(foundUsername) throw Error("The username already exists");
-
-            const salt = await bcryptjs.genSaltSync(10);
-            newUser.password = await bcryptjs.hash(password, salt); // Cipher Password
-
-            try {
-                const user = new User(newUser);
-                user.save();
-                return user;
-            } catch(error) {
-                console.log(error);
-            }
-        }
+        register: (_, { input }) => userController.register(input),
+        login: (_, { input }) => userController.login(input),
     }
 };
 
