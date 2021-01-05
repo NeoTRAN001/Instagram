fbc2d1b477a73da557057c501f63ae2e2319b158
NeoTRAN001 <neo_tran@outlook.com>
2021-01-05 14:12:45 -0600

    Toast

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { ApolloProvider } from '@apollo/client';
+import { ToastContainer } from "react-toastify";
 import client from './config/apollo';
 import Auth from './pages/Auth';
 
@@ -10,6 +11,17 @@ export default function App() {
   return (
     <ApolloProvider client={client}>
       { !auth ? <Auth /> : <h1>Estás logeado</h1> }
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </ApolloProvider>
   ); 
 }
