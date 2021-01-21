102f3bacea5174cc1dc7d1303e958a0cebffcbc8
NeoTRAN001 <neo_tran@outlook.com>
2021-01-20 18:42:11 -0600

    Add validate forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ApolloProvider } from '@apollo/client';
 import { ToastContainer } from "react-toastify";
 import client from './config/apollo';
 import Auth from './pages/Auth';
+import { getToken, setToken } from './utils/token';
 
 export default function App() {
 
   const [auth, setAuth] = useState(undefined);
+
+  useEffect(() => {
+    const token = getToken();
+    if(!token) {
+      setToken
+    }
+  }, [])
 
   return (
     <ApolloProvider client={client}>
