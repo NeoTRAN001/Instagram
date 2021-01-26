56e3b08113daa5f981c74c473fad870633b8776d
NeoTRAN001 <neo_tran@outlook.com>
2021-01-25 23:37:19 -0600

    Use useAuth

--- client/src/components/Auth/LoginForm/LoginForm.js
+++ client/src/components/Auth/LoginForm/LoginForm.js
@@ -5,12 +5,16 @@ import * as Yup from 'yup';
 import { useMutation } from '@apollo/client';
 import { LOGIN } from '../../../gql/user';
 import { setToken } from '../../../utils/token';
+import useAuth from '../../../hooks/useAuth';
 import './LoginForm.scss';
 
 export default function LoginForm() {
     const [error, setError] = useState("");
 
     const [ login ] = useMutation(LOGIN);
+
+    const auth = useAuth();
+    console.log(auth);
 
     const formik = useFormik({
         initialValues: initialValues(),
