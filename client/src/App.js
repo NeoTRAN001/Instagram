fdecf503414b33fbd9a3942279ea65a04d10acd3
NeoTRAN001 <neo_tran@outlook.com>
2021-01-30 21:58:49 -0600

    Import Home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { ApolloProvider } from '@apollo/client';
 import { ToastContainer } from "react-toastify";
 import client from './config/apollo';
 import Auth from './pages/Auth';
 import { getToken, setToken } from './utils/token';
 import AuthContext from './context/AuthContext';
+import Home from './pages/Home';
 
 export default function App() {
 
   const [auth, setAuth] = useState(undefined);
-  const authData = {
-    name: "Pablo",
-  }
-
+ 
   useEffect(() => {
     const token = getToken();
     setAuth(token ? token : null);
-  }, [])
+  }, []);
+
+  const logout = () => {
+    console.log('Cerrar sesión');
+  }
+
+  const setUser = (user) => {
+    setAuth(user);
+  }
+
+  const authData = useMemo(
+    () => ({
+      auth,
+      logout,
+      setUser
+    }),
+    [auth]
+  );
 
   return (
     <ApolloProvider client={client}>
       <AuthContext.Provider value={authData}>
-        { !auth ? <Auth /> : <h1>Estás logeado</h1> }
+        { !auth ? <Auth /> : <Home /> }
         <ToastContainer
           position="top-right"
           autoClose={5000}
