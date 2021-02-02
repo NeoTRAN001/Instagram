ecefddb6f5e9be062013a5a9b63e1e112cfc2652
NeoTRAN001 <neo_tran@outlook.com>
2021-02-02 13:29:51 -0600

    feat: decode token

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React from 'react';
+import useAuth from '../../hooks/useAuth';
 
 export default function Home() {
+    const auth = useAuth();
+    console.log(auth);
     return (
         <div>
             <h1>Estamos en la HOME</h1>
