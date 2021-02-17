7a17785174d79a2322f7e1e399c347e1b8a8d8eb
NeoTRAN001 <neo_tran@outlook.com>
2021-02-16 20:10:23 -0600

    Add Menu layout in project

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -1,16 +1,20 @@
-// Pages
+import Layout from "../layouts/LayoutBasic";
+
 import Home from '../pages/Home';
 import User from '../pages/User';
 import Error404 from '../pages/Error404';
+import LayoutBasic from "../layouts/LayoutBasic";
 
 const routes = [
     {
         path: "/",
+        layout: LayoutBasic,
         component: Home,
         exact: true
     },
     {
         path: "/:username",
+        layout: LayoutBasic,
         component: User,
         exact: true
     },
